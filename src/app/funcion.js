337b728ff0db9d8dc8d2773d30638575c9faf20d
pablorr614 <cmpsolofaru@gmail.com>
2022-12-08 02:02:29 +0100

    se añade pagina de juego

--- src/app/funcion.js
+++ src/app/funcion.js
@@ -20,3 +20,30 @@ function ghostPalabra(){
     vistaUser.forEach(link => link.style.display = 'block')
     vistaPalabra.forEach(link => link.style.display = 'none')
 }
+
+
+
+/**
+ * validores
+ */
+
+function ValidarNombre(nombre) {
+    //Validar nombre con expreciones regulares
+    var nom = nombre
+    const regexp = new RegExp(/^([A-Za-z])\w+$/)
+    var valida = regexp.test(String(nom))
+
+    return valida;
+
+}
+
+function validaVacio(valor) {
+    valor = valor.replace("&nbsp;", "");
+    valor = valor == undefined ? "" : valor;
+    if (!valor || 0 === valor.trim().length) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
